ce12183f412c21d9d2dc055f78804328f8ad5cdd
Arthur <car3man@gmail.com>
2023-09-26 01:28:59 +0300

    Minor updates, godot 3.5.2

--- app/src/main/java/io/getready/godotiac/GodotIAC.java
+++ app/src/main/java/io/getready/godotiac/GodotIAC.java
@@ -1,27 +1,55 @@
 package io.getready.godotiac;
 
 import android.app.Activity;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.collection.ArraySet;
 
 import org.godotengine.godot.Godot;
 import org.godotengine.godot.plugin.GodotPlugin;
+import org.godotengine.godot.plugin.SignalInfo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class GodotIAC extends GodotPlugin {
+
+    private final Godot godot;
+
     public GodotIAC(Godot godot) {
         super(godot);
-
-        Activity godotActivity = godot.getActivity();
-        IAC.getInstance().start(godotActivity, (payload, origin) -> {
-            Toast toast = Toast.makeText(godotActivity, "App link received!", Toast.LENGTH_LONG);
-            toast.show();
-        });
+        this.godot = godot;
     }
 
     @NonNull
     @Override
     public String getPluginName() {
         return "GodotIAC";
+    }
+
+    @NonNull
+    @Override
+    public List<String> getPluginMethods() {
+        return Arrays.asList("start", "stop", "deeplink_received");
+    }
+
+    @NonNull
+    @Override
+    public Set<SignalInfo> getPluginSignals() {
+        Set<SignalInfo> signals = new ArraySet<>();
+        signals.add(new SignalInfo("deeplink_received", String.class, String.class));
+        return signals;
+    }
+
+    public void start() {
+        Activity godotActivity = godot.getActivity();
+        IAC.getInstance().start(godotActivity, (payload, origin) -> {
+            emitSignal("deeplink_received", payload, origin);
+        });
+    }
+
+    public void stop() {
+        IAC.getInstance().stop();
     }
 }
--- app/src/main/java/io/getready/godotiac/IACActivity.java
+++ app/src/main/java/io/getready/godotiac/IACActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.net.ParseException;
+import android.util.Log;
 
 public class IACActivity extends Activity {
 
